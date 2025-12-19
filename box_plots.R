f492fd0e7c978f3b12842762ff37e009c90b58bc
RAYYANHASSAN321 <rayyanhassan1688@gmail.com>
2025-12-19 10:46:47 +0500

    new code

--- box_plots.R
+++ box_plots.R
@@ -33,3 +33,44 @@ ggplot(df , aes(x = section , y = marks , fill = section)) +
   geom_boxplot(notch = TRUE , color = "black") +
   scale_fill_manual(values = c("pink" , "lightgreen")) + 
   labs(title = "Comparison of Section A & B" , X = "Section" , y = "marks")
+
+  # --- Box Plot ---
+boxplot(marks, main = "Box plot of students marks", ylab = "Marks", 
+        col = "yellow", border = "black"
+)
+
+stats <- boxplot(marks, plot = FALSE)$stats
+text(1.2, stats, labels = round(stats, 1), col = "black")
+
+# -------------------------- Pie Chart -------------------------- #
+
+expenses = c(25000, 15000, 5000, 3000, 2000)
+categories = c("Rent", "Food", "Utilities", "Transport", "Entertainment")
+
+totals = sum(expenses)
+percentage = round((expenses / totals) * 100)
+
+label = paste(categories, "-", percentage, "%")
+
+pie(expenses, main = "Monthly budget breakdown",
+    col = rainbow(length(expenses)),
+    border = "white", labels = label
+)
+
+# --- Donut Chart ---
+pie(expenses, col = rainbow(length(expenses)),
+    main = "Donut chart"
+)
+
+symbols(0, 0, circles = 0.5, inches = FALSE, add = TRUE, bg = "white")
+
+# --- Install and Load Library ---
+install.packages("plotrix")
+library(plotrix)
+
+pie3D(
+  expenses , labels = label ,
+  main = "Monthly budget breakdown" ,
+  col= rainbow(length(expenses)) ,
+  border = "white" , explode = 0.4 , labelcex = 1.0
+)
